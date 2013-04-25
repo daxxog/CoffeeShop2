f9981f12902c72100a8145013134394727d4c2a8
David Volm <david@volminator.com>
2013-04-25 09:05:35 -0500

    WIP

--- CoffeeShop2.js
+++ CoffeeShop2.js
@@ -4,3 +4,55 @@
  * Released under Apache License, Version 2.0:
  * http://www.apache.org/licenses/LICENSE-2.0.html  
  */
+
+/* UMD LOADER: https://github.com/umdjs/umd/blob/master/returnExports.js */
+(function (root, factory) {
+    if (typeof exports === 'object') {
+        // Node. Does not work with strict CommonJS, but
+        // only CommonJS-like enviroments that support module.exports,
+        // like Node.
+        module.exports = factory();
+    } else if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        define(factory);
+    } else {
+        // Browser globals (root is window)
+        root.returnExports = factory();
+  }
+}(this, function() {
+    var express = require('express'),
+        async = require('async'),
+        socket = require('socket.io'),
+        prosess = require('prosess'),
+        http = require('http'),
+        RedisRing = require('redis-ring');
+    
+    var cs = function() {
+        this.app = express();
+        this.http = http.createServer(this.app);
+        this.io = socket.listen(this.http);
+        this.secret = 'secret';
+        
+        this.listenArgs = arguments;
+    };
+    
+    cs.prototype.ring = function(ring) {
+        this.client = new RedisRing((typeof ring == 'undefined') ? [
+            {"127.0.0.1:6379": 1} //default ring
+        ] : ring);
+        
+        return this; //chain
+    };
+    
+    cs.prototype.set = function() {
+        this.app.set.apply(this.app, arguments);
+        
+        return this; //chain
+    };
+    
+    cs.prototype.secret = function(val) {
+        this.secret = (typeof val == 'string') ? val : 'secret';
+        
+        return this;
+    };
+}));
---
+++ server.js
@@ -0,0 +1,24 @@
+/* CoffeeShop2 / server.js
+ * example of a running a static / dynamic server
+ * (c) 2012 David (daXXog) Volm ><> + + + <><
+ * Released under Apache License, Version 2.0:
+ * http://www.apache.org/licenses/LICENSE-2.0.html  
+ */
+
+var cs = require('coffeeshop'),
+    dynamic = require('./dynamic'),
+    error = require('./error'),
+    init = require('./init');
+    
+cs.ring(); //setup RedisRing
+cs.set('message', 'Hello World!'); //pass a message to dynamic
+cs.mode('production'); //set the mode to production for everything
+cs.error(error); //bind a custom error handler
+cs.error('not an error', 'yeah this is fake'); //throw a random fake error
+cs.bind(init);
+cs.bind(); //bind default directory to the app
+cs.bind('cash.js', 'npm'); //bind a local npm module to the app 
+cs.bind('./static/404.html', '404'); //bind a 404 page to the app
+cs.bind('./static2'); //bind a second directory to the app
+cs.bind(dynamic); //bind the dynamic server to the app
+cs.listen(7777); //listen on a lucky port number
